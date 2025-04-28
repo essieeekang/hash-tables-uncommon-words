836cd6577ea09257ba4341fd93cf6d4c4d3933ed
Esther Kang <estherkang.j@gmail.com>
2025-04-27 19:40:14 -0700

    adds implementation for original, challenge_1, and challenge_2

--- activity/challenge_1.py
+++ activity/challenge_1.py
@@ -1,2 +1,17 @@
 def uncommon_from_sentences(sentences):
-    pass
+    word_freq = {}
+    for sentence in sentences:
+        words = sentence.split()
+        for word in words:
+            if word not in word_freq:
+                word_freq[word] = 1
+            else:
+                word_freq[word] += 1
+
+    unique_words = []
+
+    for word in word_freq:
+        if word_freq[word] == 1:
+            unique_words.append(word)
+    
+    return unique_words
--- activity/original.py
+++ activity/original.py
@@ -1,2 +1,27 @@
 def uncommon_from_sentences(s1, s2):
-    pass
+    words = s1 + " " + s2 
+    words_list = words.split()
+    word_freq = {}
+
+    for word in words_list: 
+        if word not in word_freq:
+            word_freq[word] = 1
+        else:
+            word_freq[word] += 1
+
+    unique_words = []
+
+    for word in word_freq:
+        if word_freq[word] == 1:
+            unique_words.append(word)
+    
+    return unique_words
+
+    word_freq = {}
+    string = "this is a cat"
+    for i in range(len(string)):
+        start 
+        end 
+        if string[i] == " ":
+            word_freq[string[start:end]] = 1
+
